b98f77c6b6910d727ee0afa7b05ba75bda0996ca
hebamesluh1 <hebamesluh@gmail.com>
2023-07-31 16:52:14 +0300

    ...

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Product from "./components/Product";
+import ProductsPage from "./pages/ProductsPage";
 
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/details/:id" element={<Product/>}/>
+        <Route path="/products" element={<ProductsPage/>}/>
+        <Route path="*" element={<h1>404</h1>}/>
+      </Routes>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Categories.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import Features from './Features';
+import Title from './Title';
+
+const Categories = () => {
+    const [Category, setCategory] = useState([]);
+    useEffect(() => {
+        const fetchCategory = async () => {
+            const res = await fetch('https://fakestoreapi.com/products/categories');
+            const data = await res.json();
+            setCategory(data);
+            console.log(data);
+        }
+        fetchCategory();
+    });
+
+    if (Category.length === 0) {
+        <Title title="features" />
+        return <div>Loading ....</div>
+    } else {
+        return (
+            <>
+                <Title title="features" />
+                <Features cards={Category} />
+            </>
+        )
+    }
+}
+
+export default Categories
