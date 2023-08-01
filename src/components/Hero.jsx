1dc279d56dea6521148363d0355332a90c5fa0e1
hebamesluh1 <hebamesluh@gmail.com>
2023-08-01 19:37:31 +0300

    Add to Cart functionality

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -39,7 +39,7 @@ const Hero = () => {
                         </div>
                     </div>
                     <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-                        <img className="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600" />
+                        <img className="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600" loading="lazy" />
                     </div>
                 </div>
             </section>
